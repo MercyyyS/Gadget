5d43e87c5a89040932ee486045d9c115f4df2517
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-09-07 12:29:44 +0400

    fixes

--- src/contract/Gadget.sol
+++ src/contract/Gadget.sol
@@ -1,10 +1,9 @@
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity >=0.7.0 <0.9.0;
 
-
 interface IERC20Token {
-   function transfer(address, uint256) external returns (bool);
+    function transfer(address, uint256) external returns (bool);
 
     function approve(address, uint256) external returns (bool);
 
@@ -27,139 +26,135 @@ interface IERC20Token {
         uint256 value
     );
 }
- 
- contract   Gadgets{
-    
-    uint internal gadgetsLength = 0;
-    uint256 internal gadgetsId = 0;
-    address internal cUsdTokenAddress =   0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
-      
+contract Gadgets {
+    uint private glassLength = 0;
+    address private cUsdTokenAddress =
+        0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
     event likeGadgetEvent(address indexed userAddress, uint256 index);
     event dislikeGadgetEvent(address indexed userAddress, uint256 index);
     event deleteGadgetEvent(uint256 glassId);
-    event buyGadgetEvent(address indexed seller, address indexed buyer, uint256 index);
-     event addGadgetEvent(address indexed owner, uint256 gadgetId);  
+    event buyGadgetEvent(
+        address indexed seller,
+        address indexed buyer,
+        uint256 index
+    );
+    event addGadgetEvent(address indexed owner, uint256 gadgetId);
 
-    struct  Gadget{
+    struct Glass {
         address payable owner;
-        uint256 gadgetId;
         string image;
         string name;
         string description;
-         uint price;
-         uint likesCount;
-         
-      
+        uint price;
+        uint likesCount;
     }
-      mapping (uint =>  Gadget) internal gadgets;
-      mapping(uint256 => mapping(address => bool)) likes; // glasses liked by all users
+    mapping(uint => Glass) internal glasses;
+    mapping(uint256 => mapping(address => bool)) likes; // glasses liked by all users
 
-      //  function use to add books
-     function  addGadget(
-        string memory _image, 
-        string memory _name,
-         string memory _description,
+    /// @dev  function to use to add books
+    function addGlass(
+        string calldata _image,
+        string calldata _name,
+        string calldata _description,
         uint _price
-       
-        
-        
-    ) public {
-         uint _likesCount = 0;
-
-         gadgets [gadgetsLength] =  Gadget(
+    ) external {
+        require(bytes(_image).length > 0, "Empty image");
+        require(bytes(_name).length > 0, "Empty name");
+        require(bytes(_description).length > 0, "Empty description");
+        require(_price > 0, "Price needs to be at least one wei");
+        uint _likesCount = 0;
+        glasses[glassLength] = Glass(
             payable(msg.sender),
-            gadgetsId,
             _image,
             _name,
             _description,
             _price,
-             _likesCount
-            
-             
+            _likesCount
         );
 
-        emit addGadgetEvent(msg.sender, gadgetsId);
-        gadgetsLength++;
-        gadgetsId++;
-    
+        emit addGadgetEvent(msg.sender, glassLength);
+        glassLength++;
     }
 
-    // return glass details with key @index from glass mapping
-     function getGadget(uint _index) public view returns (
-        address payable,
-        uint256,
-        string memory,
-        string memory,  
-        string memory,
-        uint,
-        uint
-        
-      
-    ) {
-        return (  
-            gadgets[_index].owner,
-               gadgets[_index].gadgetId,
-                gadgets[_index].image,
-              gadgets[_index].name, 
-              gadgets[_index].description,
-                gadgets[_index].price,
-                gadgets[_index].likesCount
-
-
-                   
+    /// @return glass details with key @index from glass mapping
+    function getGlass(uint _index)
+        public
+        view
+        returns (
+            address payable,
+            string memory,
+            string memory,
+            string memory,
+            uint,
+            uint
+        )
+    {
+        return (
+            glasses[_index].owner,
+            glasses[_index].image,
+            glasses[_index].name,
+            glasses[_index].description,
+            glasses[_index].price,
+            glasses[_index].likesCount
         );
     }
 
-    // delete glass with key @_index from books mapping
-    function RemoveGlasses(uint _index) external {
-	        require(msg.sender == gadgets[_index].owner, "Only owner can delete ");      
-            uint256 gadgetId = gadgets[_index].gadgetId;      
-            gadgets[_index] = gadgets[gadgetsLength - 1];// 
-            delete gadgets[gadgetsLength - 1]; 
-            gadgetsLength--; // 
+    /// @dev delete glass with key @_index from books mapping
+    function removeGlass(uint _index) external {
+        require(msg.sender == glasses[_index].owner, "Only owner can delete ");
+        glasses[_index] = glasses[glassLength - 1];
+        delete glasses[glassLength - 1];
+        glassLength--;
 
-            emit deleteGadgetEvent(gadgetId);
-	    }
+        emit deleteGadgetEvent(_index);
+    }
 
-        
-          function buyGlass(uint _index) public payable  {
+    /**
+     * @dev allow users to buy a glass from the platform
+     */
+    function buyGlass(uint _index) public payable {
+        Glass storage currentGlass = glasses[_index];
         require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
-            msg.sender,
-            gadgets[_index].owner,
-             gadgets[_index].price
-          ),
-          "Transfer failed."
+            currentGlass.owner != msg.sender,
+            "You can't buy your own glass"
+        );
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                glasses[_index].owner,
+                glasses[_index].price
+            ),
+            "Transfer failed."
         );
 
-        address seller = gadgets[_index].owner;
-        address buyer = msg.sender;
+        address seller = glasses[_index].owner;
 
-       gadgets[_index].owner = payable(msg.sender);
+        glasses[_index].owner = payable(msg.sender);
 
-       emit buyGadgetEvent(seller, buyer, _index);
-         
+        emit buyGadgetEvent(seller, msg.sender, _index);
     }
- 
-     function Like (uint _index)public{
-         bool hasLiked = likes[gadgets[_index].gadgetId][payable(msg.sender)];
-    
-         if (hasLiked) {
-             likes[gadgets[_index].gadgetId][payable(msg.sender)] = false;
-            gadgets[_index].likesCount--; 
-            emit dislikeGadgetEvent(msg.sender, _index);
-         } else {
-       
-             likes[gadgets[_index].gadgetId][payable(msg.sender)] = true;
-             gadgets[_index].likesCount++;
-             emit likeGadgetEvent(msg.sender, _index);
-         }        
-    } 
 
-     
-    function getgadgetsLength() public view returns (uint) {
-        return (gadgetsLength);
+    /**
+     * @dev allow users to like or unlike a glass
+     * @notice this function is used to like or dislike
+     */
+    function Like(uint _index) public {
+        bool hasLiked = likes[_index][msg.sender];
+
+        if (hasLiked) {
+            likes[_index][msg.sender] = false;
+            glasses[_index].likesCount--;
+            emit dislikeGadgetEvent(msg.sender, _index);
+        } else {
+            likes[_index][msg.sender] = true;
+            glasses[_index].likesCount++;
+            emit likeGadgetEvent(msg.sender, _index);
+        }
+    }
+
+    function getGlassesLength() public view returns (uint) {
+        return (glassLength);
     }
 }
-
